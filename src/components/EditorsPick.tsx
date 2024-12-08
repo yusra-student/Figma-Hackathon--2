29afa98aff1d0a2413ef19467b838cfe67e2a318
yusra-student <abdulwaheedabdul4@gmail.com>
2024-12-09 00:19:40 +0500

    first commit

--- src/components/EditorsPick.tsx
+++ src/components/EditorsPick.tsx
@@ -17,7 +17,7 @@ export default function EditorsPick() {
 
                     {/* Men's */}
                     <div className="col-span-1 lg:col-span-2 row-span-2 md:row-span-6 h-[500px] lg:h-full relative">
-                        <Image src={"/images/editors-pick/image.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} />
+                        <Image src={"/images/editors-Pick/image.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} />
                         <Link href={"/shop"}>
                         <button className="rounded-sm w-[170px] h-[48px] bg-white text-base font-montserrat font-bold absolute bottom-5 left-5">MEN</button>
                         </Link>
@@ -25,7 +25,7 @@ export default function EditorsPick() {
 
                     {/* Women's  */}
                     <div className="col-span-1 row-span-2 md:row-span-6 h-[500px] lg:h-full relative">
-                        <Image src={"/images/editors-pick/image1.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
+                        <Image src={"/images/editors-Pick/image1.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
                         <Link href={"/shop"}>
                         <button className="rounded-sm w-[170px] h-[48px] bg-white text-base font-montserrat font-bold absolute bottom-5 left-5">WOMEN</button>
                         </Link>
@@ -37,7 +37,7 @@ export default function EditorsPick() {
                         {/* one */}
                         <div className="mb-3 col-span-1 row-span-1 md:row-span-3 h-[41%] relative">
                             
-                            <Image src={"/images/editors-pick/image2.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
+                            <Image src={"/images/editors-Pick/image2.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
 
                             <Link href={"/shop"}>
                             <button className="rounded-sm w-[170px] h-[48px] bg-white text-base font-montserrat font-bold absolute bottom-5 left-5">ACCESSORIES</button>
@@ -47,7 +47,7 @@ export default function EditorsPick() {
                         {/* two */}
                         <div className="col-span-1 row-span-1 md:row-span-3 h-[41%] relative">
                             
-                            <Image src={"/images/editors-pick/image3.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
+                            <Image src={"/images/editors-Pick/image3.png"} alt="1" className="w-full h-full object-cover" width={100} height={100} sizes={"100vw"} />
                             <Link href={"/shop"}>
                             <button className="rounded-sm w-[170px] h-[48px] bg-white text-base font-montserrat font-bold absolute bottom-5 left-5">ACCESSORIES</button>
                             </Link>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,22 +1,38 @@
-import Image from "next/image"
-import heroImg from "../../public/images/hero-sec.jpg"
-import Link from "next/link"
+import Image from "next/image";
+import heroImg from "../../public/images/hero-sec.jpg";
+import Link from "next/link";
 
 export default function HeroSection() {
     return (
-        <div className={`mb-20 w-screen relative flex flex-col justify-center items-center `}>
-            <Image src={heroImg} alt="hero" className="w-screen h-screen  object-cover" />
+        <div className="mb-20 w-screen relative flex flex-col justify-center items-center">
+            {/* Responsive Background Image */}
+            <div className="w-full h-[60vh] sm:h-[75vh] md:h-screen relative">
+                <Image
+                    src={heroImg}
+                    alt="hero"
+                    className="object-cover"
+                    fill
+                    priority
+                />
+            </div>
 
-            <div className="font-montserrat text-white absolute left-[20%] flex flex-col justify-center items-start">
-                <p className=" font-bold text-base mb-10">SUMMER 2020</p>
-                <h1 className=" font-bold text-3xl md:text-5xl mb-7 md:mb-10">NEW COLLECTION</h1>
-                <p className=" text-base md:text-xl w-[350px] md:w-[376px] mb-7 text-[#fafafa]">We know how large objects will act, 
-                but things on a small scale.</p>
-
+            {/* Content */}
+            <div className="absolute flex flex-col items-start justify-center text-white font-montserrat left-4 right-4 sm:left-[10%] md:left-[20%] top-[20%]">
+                <p className="font-bold text-xs sm:text-sm md:text-base mb-5 sm:mb-7">
+                    SUMMER 2020
+                </p>
+                <h1 className="font-bold text-2xl sm:text-3xl md:text-5xl mb-5 sm:mb-7">
+                    NEW COLLECTION
+                </h1>
+                <p className="text-sm sm:text-base md:text-xl text-[#fafafa] w-full sm:w-[300px] md:w-[376px] mb-5 sm:mb-7">
+                    We know how large objects will act, but things on a small scale.
+                </p>
                 <Link href={"/shop"}>
-                <button className="rounded-sm w-[221px] h-[62px] bg-[#2DC071]">SHOP NOW</button>
-                </Link>   
+                    <button className="w-full sm:w-[200px] md:w-[221px] h-[50px] md:h-[62px] bg-[#2DC071] text-sm sm:text-base rounded-sm">
+                        SHOP NOW
+                    </button>
+                </Link>
             </div>
         </div>
-    )
+    );
 }
